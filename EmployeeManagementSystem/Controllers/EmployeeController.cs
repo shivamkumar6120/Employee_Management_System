4138f4afee9da3b6113c512b467797b12cc2fd56
shivamkumar6120 <nirajshivam6120@gmail.com>
2025-12-31 15:23:24 +0530

    EmployeeConroller.cs created with List()

---
+++ EmployeeManagementSystem/Controllers/EmployeeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        //Views/Employee/List.cshtml
+
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
