0a26c6dda84331754128b740e8d1f0022e9e15ff
shivamkumar6120 <nirajshivam6120@gmail.com>
2025-12-31 17:05:13 +0530

    Employee Model is added

---
+++ EmployeeManagementSystem/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagementSystem.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+
+        public string Email { get; set;  }
+
+        [StringLength (25)]
+        public string? Department { get; set;  }
+
+        [Range(0, 10000000)]
+        public decimal Salary { get; set;  }
+    }
+}
