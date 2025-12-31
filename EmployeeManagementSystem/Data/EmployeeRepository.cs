afb63dd3251d50430b955c9a3f02d183f08303f2
shivamkumar6120 <nirajshivam6120@gmail.com>
2025-12-31 18:02:19 +0530

    m

---
+++ EmployeeManagementSystem/Data/EmployeeRepository.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;   
+using EmployeeManagementSystem.Models;
+
+
+namespace EmployeeManagementSystem.Data
+{
+    public class EmployeeRepository
+    {
+
+        private readonly string _connectionString;
+
+        public EmployeeRepository(IConfiguration configuration) // IConfiguration - Reads appsettings.json
+        {
+            _connectionString = configuration.GetConnectionString("EmployeeDBConnection")
+                ?? throw new InvalidOperationException("EmployeeDBConnection is not configured.");
+
+            //_connectionString - DB connection 
+        }
+
+      
+
+        public List<Employee> GetAllEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            using (SqlConnection connection = new SqlConnection(_connectionString))// SqlConnection  - Connect to SQL Server and using is like try with resources in java
+            {
+                string query = "SELECT Id, Name, Email, Department, Salary FROM Employees";
+                SqlCommand command = new SqlCommand(query, connection); // SqlCommand - Execute SQL
+                connection.Open();
+
+
+                SqlDataReader reader = command.ExecuteReader();//  SqlDataReader - Read rows one by one
+                while (reader.Read())
+                {
+                    Employee emp = new Employee
+                    {
+                        Id = reader.GetInt32("Id"),
+                        Name = reader.GetString("Name"),
+                        Email = reader.GetString("Email"),
+                        Department = reader.IsDBNull("Department")
+                            ? null
+                            : reader.GetString("Department"),
+                        Salary = reader.GetDecimal("Salary")
+
+                    };
+                    employees.Add(emp);
+                }
+            }
+            return employees;
+
+        }
+    }
+}
