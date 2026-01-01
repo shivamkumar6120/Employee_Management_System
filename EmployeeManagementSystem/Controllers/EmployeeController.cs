fa9d4df2a1dd4ab9c27fbd57d7e89a4cfea28800
shivamkumar6120 <nirajshivam6120@gmail.com>
2026-01-01 14:28:46 +0530

    Create and Edit and Delete  method in EmployeeController.cs file

--- EmployeeManagementSystem/Controllers/EmployeeController.cs
+++ EmployeeManagementSystem/Controllers/EmployeeController.cs
@@ -32,5 +32,83 @@ namespace EmployeeManagementSystem.Controllers
             return View(employees);
         }
 
+
+
+         //GET: /Employee/Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Employee/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Employee employee)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(employee);
+            }
+
+            _repository.AddEmployee(employee);
+            // PRG Pattern: Post → Redirect → Get
+            return RedirectToAction("List");    
+        }
+
+        // GET: /Employee/Edit/5
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var employee = _repository.GetEmployeeById(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // POST: /Employee/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(employee);
+            }
+
+            _repository.UpdateEmployee(employee);
+
+            return RedirectToAction("List");
+        }
+
+
+        // GET: /Employee/Delete/5
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var employee = _repository.GetEmployeeById(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // POST: /Employee/Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _repository.DeleteEmployee(id);
+            return RedirectToAction("List");
+        }
+
+
     }
 }
